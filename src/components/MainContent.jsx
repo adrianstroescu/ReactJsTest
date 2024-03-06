c3a7353e99e23896c72802d2d291d3911479bb35
Adrian21 <stroescuadrian@gmail.com>
2024-03-06 15:26:40 +0200

    march 6 part 2

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,14 @@
 import { Flex } from 'antd';
 import React from 'react'
 import Banner from './Banner';
+import ProductsLists from './ProductsLists';
 
 const MainContent = () => {
   return (
     <div style={{flex: 1}}>
         <Flex vertical gap="2.3rem">
-            <Banner></Banner>
+            <Banner />
+            <ProductsLists />
         </Flex>
     </div>
   )
--- src/components/ProductsLists.jsx
+++ src/components/ProductsLists.jsx
@@ -1,9 +1,32 @@
+import {Button, Card, Flex ,Typography, Image} from 'antd'
+import plantData from '../plantData';
 import React from 'react'
 
+
+const {Meta} = Card;
 const ProductsLists = () => {
   return (
-    <div>ProductsLists</div>
+    <>
+    <Flex align='center' justify='space-between'>
+        <Typography.Title level={3}strong className='primary--color' >
+            My Listing
+        </Typography.Title>
+        <Button type='link' className='gray--color'>
+            View All
+        </Button>
+
+    </Flex>
+
+    <Flex align='center' gap="large">
+        {plantData.map((plant) => (
+            <Card key={plant.id} hoverable className='plant-card'>
+                <Image src={plant.picture} style={{width: '130px'}} />
+            </Card>
+        ))}
+
+    </Flex>
+    </>
   )
 }
 
-export default ProductsLists
+export default ProductsLists;
